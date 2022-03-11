4aecafa9a05e518cf3378fa683ce7666715d3dcc
pavelvasilev8500 <61418223+pavelvasilev8500@users.noreply.github.com>
2022-03-11 14:27:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,19 +4,17 @@ const mongoose = require('mongoose');
 const app = experss();
 const bodyParser = require('body-parser');
 
-const db = mongoose.connect('mongodb+srv://user:******@datacluster.1j5iz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+const db = mongoose.connect('mongodb+srv://admin:44224422@datacluster.1j5iz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 const ifaces = require('os').networkInterfaces();
 const Data = require('./models/dataModel');
 const CtcontrolRouter = require('./routes/CtcontrolRouter')(Data);
 const controlData = require('./models/controldataModel');
 const { Console } = require('console');
-const CtsystemRouter = require('./routes/CtsystemRouter')(controlData);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api', CtcontrolRouter);
-app.use('/api', CtsystemRouter);
 
 const localhost = Object.keys(ifaces).reduce((host, ifname) => {
     const iface = ifaces[ifname].find((iface) => !(iface.family !== 'IPv4' || iface.internal !== false));
