da698e2f6bad5f51e4bcecff8c2d9c143be555d9
pavelvasilev8500 <61418223+pavelvasilev8500@users.noreply.github.com>
2022-03-12 01:15:34 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,9 +7,7 @@ const bodyParser = require('body-parser');
 const db = mongoose.connect('mongodb+srv://admin:44224422@datacluster.1j5iz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 const ifaces = require('os').networkInterfaces();
 const Data = require('./models/dataModel');
-const CtcontrolRouter = require('./routes/CtcontrolRouter')(Data);
-const controlData = require('./models/controldataModel');
-const { Console } = require('console');
+const CtcontrolRouter = require('./routes/CtcontrolRouter')(Data)   
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -17,12 +15,11 @@ app.use(bodyParser.json());
 app.use('/api', CtcontrolRouter);
 
 const localhost = Object.keys(ifaces).reduce((host, ifname) => {
-    const iface = ifaces[ifname].find((iface) => !(iface.family !== 'IPv4' || iface.internal !== false));
-    return iface ? iface.address : host;
+  const iface = ifaces[ifname].find((iface) => !(iface.family !== 'IPv4' || iface.internal !== false));
+  return iface ? iface.address : host;
 }, '127.0.0.1');
-
-const port = 8282;
+const port = process.env.PORT || 8282;
 
 app.listen(port, () => {
-    console.log(`Server starting...\nip: ${localhost}\nport: ${port}`);
+  console.log(`Server starting...\nip: ${localhost}\nport: ${port}`);
 });
