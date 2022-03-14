883b23eeac483d97511e81c01e03f027e3b11286
pavelvasilev8500 <61418223+pavelvasilev8500@users.noreply.github.com>
2022-03-14 15:30:13 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const experss = require('express');
+const express = require('express');
 const mongoose = require('mongoose');
 
-const app = experss();
+const app = express();
 const bodyParser = require('body-parser');
 
-const db = mongoose.connect('mongodb+srv://admin:44224422@datacluster.1j5iz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+const db = mongoose.connect('mongodb+srv://admin:44224422@datacluster.1j5iz.mongodb.net/CtcontrolDatabase?retryWrites=true&w=majority');
 const ifaces = require('os').networkInterfaces();
-const Data = require('./models/dataModel');
-const CtcontrolRouter = require('./routes/CtcontrolRouter')(Data)   
+const ClientData = require('./models/ClientData');
+const SystemData = require('./models/SystemData');
+const ClientDataRouter = require('./routes/ClientDataRouter')(ClientData);
+const SystemDataRouter = require('./routes/SystemDataRouter')(SystemData);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/api', CtcontrolRouter);
+app.get('/', (req, res) => {
+    return res.sendFile('./Views/StartView.html', { root: __dirname });
+});
+app.use('/clientdata', ClientDataRouter);
+app.use('/systemdata', SystemDataRouter);
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || 'Something went wrong. Try again later';
+    res.status(status).json({ message });
+});
 
 const localhost = Object.keys(ifaces).reduce((host, ifname) => {
   const iface = ifaces[ifname].find((iface) => !(iface.family !== 'IPv4' || iface.internal !== false));
